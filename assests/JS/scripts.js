c1e98dbb8af83b141b67bca9a643fc775f066aab
agomez80 <a.gomez8040@gmail.com>
2024-01-18 15:52:09 -0600

    First Portfolio

---
+++ assests/JS/scripts.js
@@ -0,0 +1,8 @@
+// LEAFLET//
+
+var map = L.map('map').setView([29.749907,-95.358421], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
